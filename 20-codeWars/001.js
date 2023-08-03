89a931a5217f137834dd41b8ac82aa05b0be9d95
rasamkamyar <rasamkamyar@gmail.com>
2023-08-03 11:30:56 +0330

    first edit

---
+++ 20-codeWars/001.js
@@ -0,0 +1,18 @@
+function stringy(size) {
+    debugger;
+  sum = "";
+
+  for (i = 0; i < size; i++) {
+    if (i % 2 == 0) {
+      sum += "1";
+    } else {
+      sum += "0";
+    }
+  }
+
+  return sum;
+}
+
+
+
+
