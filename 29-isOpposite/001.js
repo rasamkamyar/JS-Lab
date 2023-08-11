b532b54618dfd65b8ebaace572127c15b4f31b83
rasamkamyar <rasamkamyar@gmail.com>
2023-08-11 11:19:04 +0330

    init

---
+++ 29-isOpposite/001.js
@@ -0,0 +1,17 @@
+function isOpposite(s1, s2) {
+  
+    if (s1 === "") return false;
+
+  let opposite = "";
+
+  for (let i = 0; i < s1.length; i++) {
+    if (s1[i].toUpperCase() === s1[i]) {
+      opposite += s1[i].toLowerCase();
+    } else {
+      opposite += s1[i].toUpperCase();
+    }
+  }
+  return opposite === s2 ? true : false;
+}
+
+isOpposite();
