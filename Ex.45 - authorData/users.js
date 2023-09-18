563de10f5b3595c1a12516672d7c1f1f06d5612a
rasamkamyar <rasamkamyar@gmail.com>
2023-09-18 15:58:16 +0330

    init

--- Ex.45 - authorData/users.js
+++ Ex.45 - authorData/users.js
@@ -1,36 +1,48 @@
+let root = document.getElementById("root");
 let userReq = new XMLHttpRequest();
 userReq.open("GET", "https://jsonplaceholder.ir/users");
 
 function getUsers() {
-let userData =  JSON.parse(userReq.responseText);
-renderUser(userData)
+  let userData = JSON.parse(userReq.responseText);
+  renderUser(userData);
 }
 
 function renderUser(items) {
-let temp = items.map((item) => {
-    const{id,name,username,email,password,avatar,address,phone,website,company} = item
-    return `<div>    
-    ${id},
-    ${name},
-    ${username},
-    ${email},
-    ${password},
+  let temp = items.map((item) => {
+    const {
+      id,
+      name,
+      username,
+      email,
+      password,
+      avatar,
+      address,
+      phone,
+      website,
+      company,
+    } = item;
+    return `<div class = "card">    
+    <span>${id} - ,
+    <h1>${name}</h1>,
+    <h2>${username}</h2>
+    </span>,
+    <span>${email}</span>,
+    <span>${password}</span>,
     ${avatar},
     ${address},
-    ${phone},
-    ${website},
-    ${company},
-  </div>
-        
-        `;
+    <div class = footer>
+    <span>${phone}</span>,
+    <span>${website}</span>,
+    <span>${company}</span>,
+    </div>
+  </div>`;
   });
 
-  document.body.innerHTML = temp.join("")
+  root.innerHTML = temp.join(" ");
 }
 
 userReq.addEventListener("load", getUsers);
 userReq.send();
-
 
 // {
 //     "id": 1,
